cae14a905c39ef0ed0703f285302ef53c57bb4ed
Shawn <496224211@qq.com>
2025-03-13 18:49:25 +0800

    add home page

---
+++ src/views/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header class="h-12 px-4 bg-white grid grid-cols-[40px,1fr,40px] items-center">
+      <img alt="Vue logo" class="w-8" src="@/assets/logo.svg" />
+
+      <nav class="flex justify-center gap-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+      <div></div>
+    </header>
+
+    <div
+      class="h-[calc(100vh-theme(height.12))] grid grid-cols-[400px,1fr,400px] bg-gray-100 p-4 gap-4"
+    >
+      <aside class="size-full bg-white rounded-lg"></aside>
+      <main class="w-full h-[1080px] bg-white rounded-lg"></main>
+      <aside class="size-full bg-white rounded-lg"></aside>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
